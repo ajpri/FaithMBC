ec7f29b4ceee7acc3a3dd9d388b9788bb3afe386
Austin Pritchett <austin@pritchett.info>
2019-09-11 09:32:38 -0500

    Nav js readability

--- nav.js
+++ nav.js
@@ -1,23 +1,23 @@
 function hashHandler() {
-          console.log('The hash has changed!');
-          var hash = location.hash;
-          if(hash=="#doctrine"){
-            var client = new XMLHttpRequest();
-            client.open('GET', '/doctrine.html');
-            client.onreadystatechange = function() {
-              document.getElementById("content").innerHTML=client.responseText;
-            }
-            client.send();
-          }else if(hash == "#history"){
-            var client = new XMLHttpRequest();
-            client.open('GET', '/history.html');
-            client.onreadystatechange = function() {
-              document.getElementById("content").innerHTML=client.responseText;
-            }
-            client.send();
-          }else{
-            document.getElementById("content").innerHTML="";
-          }
+    console.log('The hash has changed!');
+    var hash = location.hash;
+    if(hash=="#doctrine"){
+        var client = new XMLHttpRequest();
+        client.open('GET', '/doctrine.html');
+        client.onreadystatechange = function() {
+            document.getElementById("content").innerHTML=client.responseText;
         }
+        client.send();
+    }else if(hash == "#history"){
+        var client = new XMLHttpRequest();
+        client.open('GET', '/history.html');
+        client.onreadystatechange = function() {
+            document.getElementById("content").innerHTML=client.responseText;
+        }
+        client.send();
+    }else{
+        document.getElementById("content").innerHTML="";
+    }
+} //End Function
 
-        window.addEventListener('hashchange', hashHandler, false);
+window.addEventListener('hashchange', hashHandler, false);
